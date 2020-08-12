9f2a9fb0f609ef3c0fb35b31c6531cae43d6245f
Arthur Kenichi Condino Odaguiri <46831317+Arthur-Kenichi-Condino@users.noreply.github.com>
2020-08-12 01:22:18 -0300

    input

--- Assets/AKCondinoO/MainCamera.cs
+++ Assets/AKCondinoO/MainCamera.cs
@@ -15,9 +15,22 @@ if(field.GetValue(null)is object[]command){
 
 }    
 }
+foreach(FieldInfo field in typeof(Enabled).GetFields(BindingFlags.Public|BindingFlags.Static)){
+
+    
+    Debug.LogWarning(field.FieldType);
+
+
+}
 }
 void Update(){
 }
+}
+public static class Enabled{
+public static readonly bool[]FORWARD ={false,false};
+public static readonly bool[]BACKWARD={false,false};
+public static readonly bool[]RIGHT   ={false,false};
+public static readonly bool[]LEFT    ={false,false};
 }
 public static class Commands{
 public static object[]FORWARD ={KeyCode.W,"activeHeld"};
