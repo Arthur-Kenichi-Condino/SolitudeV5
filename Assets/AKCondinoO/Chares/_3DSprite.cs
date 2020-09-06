f9e2097d41b40d2db660cc284ff41949b38e974e
Arthur Kenichi Condino Odaguiri <46831317+Arthur-Kenichi-Condino@users.noreply.github.com>
2020-09-06 03:02:41 -0300

    ALARM

---
+++ Assets/AKCondinoO/Chares/_3DSprite.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class _3DSprite:AI{
+[NonSerialized]protected Animator animator;
+protected override void Awake(){
+                   base.Awake();
+animator=GetComponentInChildren<Animator>();
+}
+}
