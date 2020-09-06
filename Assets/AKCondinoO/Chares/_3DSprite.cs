507b2d98c123cddd98aca565a1118e6177d448d2
Arthur Kenichi Condino Odaguiri <46831317+Arthur-Kenichi-Condino@users.noreply.github.com>
2020-09-06 06:06:08 -0300

    Update _3DSprite.cs

--- Assets/AKCondinoO/Chares/_3DSprite.cs
+++ Assets/AKCondinoO/Chares/_3DSprite.cs
@@ -8,8 +8,15 @@ protected override void Awake(){
                    base.Awake();
 animator=GetComponentInChildren<Animator>();
 }
+[NonSerialized]Vector3 _forward,_cameraForward,_forwardFromCameraToSprite;[NonSerialized]bool _back;
 protected override void LateUpdate(){
                    base.LateUpdate();
 animator.transform.rotation=Quaternion.LookRotation((Camera.main.transform.position-animator.transform.position).normalized,Vector3.up);
+_forward=Vector3.Scale(transform.eulerAngles,Vector3.up);
+_forward=Quaternion.Euler(_forward)*Vector3.forward;
+_cameraForward=Vector3.Scale(Camera.main.transform.eulerAngles,Vector3.up);
+_cameraForward=Quaternion.Euler(_cameraForward)*Vector3.forward;
+_forwardFromCameraToSprite=(transform.position-Camera.main.transform.position).normalized;
+_back=Vector3.Angle(_forward,_forwardFromCameraToSprite)<=90;
 }
 }
