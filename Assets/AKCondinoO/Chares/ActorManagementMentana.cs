cfe5c22fe5a03d65266b74c8eec6348f0a273ff5
Arthur Kenichi Condino Odaguiri <46831317+Arthur-Kenichi-Condino@users.noreply.github.com>
2020-09-13 01:08:37 -0300

    Update ActorManagementMentana.cs

--- Assets/AKCondinoO/Chares/ActorManagementMentana.cs
+++ Assets/AKCondinoO/Chares/ActorManagementMentana.cs
@@ -2,4 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 public class ActorManagementMentana:MonoBehaviour{
+[SerializeField]GameObject[]actorsPrefabs;[SerializeField]int[]actorsMaxInstantiations;
+void Awake(){
+if(actorsPrefabs!=null&&actorsMaxInstantiations!=null){
+}
+}
 }
