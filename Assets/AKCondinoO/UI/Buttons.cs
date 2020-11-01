1d547cb6060a4850d9b52b7638a1b4bfcd9fedfa
Arthur Kenichi Condino Odaguiri <46831317+Arthur-Kenichi-Condino@users.noreply.github.com>
2020-11-01 02:56:51 -0300

    TO DO button f2buy script and do some of the UI

--- Assets/AKCondinoO/UI/Buttons.cs
+++ Assets/AKCondinoO/UI/Buttons.cs
@@ -6,6 +6,6 @@ using UnityEngine.UI;
 public class Buttons:MonoBehaviour{
 [NonSerialized]Button button;[NonSerialized]Image image;
 private void Awake(){   
-button=GetComponent<Button>();image=button.image;image.alphaHitTestMinimumThreshold=0.5f;
+button=GetComponent<Button>();image=button.image;image.alphaHitTestMinimumThreshold=1f;
 }
 }
