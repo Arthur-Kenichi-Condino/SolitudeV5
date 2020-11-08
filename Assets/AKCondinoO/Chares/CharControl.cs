6400b7358690eeed849fdd72a371e9699bd7faac
Arthur Kenichi Condino Odaguiri <46831317+Arthur-Kenichi-Condino@users.noreply.github.com>
2020-11-08 01:37:32 -0300

    UI IS TOO MUCH BORING
    >.<
    
    but we are getting somewhere...

--- Assets/AKCondinoO/Chares/CharControl.cs
+++ Assets/AKCondinoO/Chares/CharControl.cs
@@ -14,20 +14,20 @@ protected override void Awake(){
 CamFollowableNode=MainCamera.CamFollowables.AddLast(this);
 }
 protected override void ProcessMovementInput(){
-if(!Enabled.PAUSE[0]){
+if(!(bool)Enabled.PAUSE[0]){
 if(BeingCamFollowed){
 if(IsGrounded||!HittingWall){
 #region FORWARD BACKWARD
-    if(Enabled.FORWARD [0]){inputMoveSpeed.z+=InputMoveAcceleration.z;Autonomous=AutonomyDelayAfterControl;} 
-    if(Enabled.BACKWARD[0]){inputMoveSpeed.z-=InputMoveAcceleration.z;Autonomous=AutonomyDelayAfterControl;}
-        if(!Enabled.FORWARD[0]&&!Enabled.BACKWARD[0]){inputMoveSpeed.z=0;}
+    if((bool)Enabled.FORWARD [0]){inputMoveSpeed.z+=InputMoveAcceleration.z;Autonomous=AutonomyDelayAfterControl;} 
+    if((bool)Enabled.BACKWARD[0]){inputMoveSpeed.z-=InputMoveAcceleration.z;Autonomous=AutonomyDelayAfterControl;}
+        if(!(bool)Enabled.FORWARD[0]&&!(bool)Enabled.BACKWARD[0]){inputMoveSpeed.z=0;}
             if( inputMoveSpeed.z>InputMaxMoveSpeed.z){inputMoveSpeed.z= InputMaxMoveSpeed.z;}
             if(-inputMoveSpeed.z>InputMaxMoveSpeed.z){inputMoveSpeed.z=-InputMaxMoveSpeed.z;}
 #endregion
 #region RIGHT LEFT
-    if(Enabled.RIGHT   [0]){inputMoveSpeed.x+=InputMoveAcceleration.x;Autonomous=AutonomyDelayAfterControl;} 
-    if(Enabled.LEFT    [0]){inputMoveSpeed.x-=InputMoveAcceleration.x;Autonomous=AutonomyDelayAfterControl;}
-        if(!Enabled.RIGHT[0]&&!Enabled.LEFT[0]){inputMoveSpeed.x=0;}
+    if((bool)Enabled.RIGHT   [0]){inputMoveSpeed.x+=InputMoveAcceleration.x;Autonomous=AutonomyDelayAfterControl;} 
+    if((bool)Enabled.LEFT    [0]){inputMoveSpeed.x-=InputMoveAcceleration.x;Autonomous=AutonomyDelayAfterControl;}
+        if(!(bool)Enabled.RIGHT[0]&&!(bool)Enabled.LEFT[0]){inputMoveSpeed.x=0;}
             if( inputMoveSpeed.x>InputMaxMoveSpeed.x){inputMoveSpeed.x= InputMaxMoveSpeed.x;}
             if(-inputMoveSpeed.x>InputMaxMoveSpeed.x){inputMoveSpeed.x=-InputMaxMoveSpeed.x;}
 #endregion
@@ -42,7 +42,7 @@ inputViewRotationEuler.x=inputViewRotationEuler.x%360;
 inputViewRotationEuler.y=inputViewRotationEuler.y%360;
 if(Enabled.MOUSE_ROTATION_DELTA_Y[0]!=0||Enabled.MOUSE_ROTATION_DELTA_X[0]!=0)Autonomous=AutonomyDelayAfterControl;
 #endregion
-if(IsGrounded&&(Jump||(Jump=Enabled.JUMP[0]!=Enabled.JUMP[1]))){
+if(IsGrounded&&(Jump||(Jump=(bool)Enabled.JUMP[0]!=(bool)Enabled.JUMP[1]))){
 inputMoveSpeed.y=InputMoveAcceleration.y;Autonomous=AutonomyDelayAfterControl;
 }else{
 inputMoveSpeed.y=0;
--- Assets/AKCondinoO/MainCamera.cs
+++ Assets/AKCondinoO/MainCamera.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using AKCondinoO.Voxels;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
 using UnityEngine;
 public class MainCamera:SimActor{
-public static Dictionary<string,object[]>AllCommands=new Dictionary<string,object[]>();public static Dictionary<string,bool[]>AllStates=new Dictionary<string,bool[]>();
+public static Dictionary<string,object[]>AllCommands=new Dictionary<string,object[]>();public static Dictionary<string,object[]>AllStates=new Dictionary<string,object[]>();
 public static readonly LinkedList<iCamFollowable>CamFollowables=new LinkedList<iCamFollowable>();public iCamFollowable CamFollowing=null;
 protected override void Awake(){
                    base.Awake();
@@ -15,7 +16,7 @@ AllCommands.Add(field.Name,command);
 }    
 }
 foreach(FieldInfo field in typeof(Enabled).GetFields(BindingFlags.Public|BindingFlags.Static)){
-if(field.GetValue(null)is bool[]state){
+if(field.GetValue(null)is object[]state){
 if(LOG&&LOG_LEVEL<=1)Debug.Log("add command state status fields:"+field.Name);
 AllStates.Add(field.Name,state);
 }
@@ -32,9 +33,9 @@ ProcessGameCommands();
 
 Escape=Input.GetKey(KeyCode.Escape)||Input.GetKeyDown(KeyCode.Escape)||Input.GetKeyUp(KeyCode.Escape);
 foreach(var command in AllCommands){UpdateCommandState(command);}
-Enabled.PAUSE[0]=Enabled.PAUSE[0]||Escape||!Focus;
-if(Enabled.PAUSE[0]!=Enabled.PAUSE[1]){
-    if(Enabled.PAUSE[0]){
+Enabled.PAUSE[0]=(bool)Enabled.PAUSE[0]||Escape||!Focus;
+if((bool)Enabled.PAUSE[0]!=(bool)Enabled.PAUSE[1]){
+    if((bool)Enabled.PAUSE[0]){
 Cursor.visible=true;
 Cursor.lockState=CursorLockMode.None;
     }else{
@@ -44,9 +45,9 @@ Cursor.lockState=CursorLockMode.Locked;
 }
 Enabled.MOUSE_ROTATION_DELTA_X[1]=Enabled.MOUSE_ROTATION_DELTA_X[0];Enabled.MOUSE_ROTATION_DELTA_X[0]=Commands.ROTATION_SENSITIVITY_X*Input.GetAxis("Mouse X");
 Enabled.MOUSE_ROTATION_DELTA_Y[1]=Enabled.MOUSE_ROTATION_DELTA_Y[0];Enabled.MOUSE_ROTATION_DELTA_Y[0]=Commands.ROTATION_SENSITIVITY_Y*Input.GetAxis("Mouse Y");
-if(!Enabled.PAUSE[0]){
-if(Enabled.SWITCH_CAMERA_MODE[0]!=Enabled.SWITCH_CAMERA_MODE[1]){
-if(Enabled.SWITCH_CAMERA_MODE[0]){
+if(!(bool)Enabled.PAUSE[0]){
+if((bool)Enabled.SWITCH_CAMERA_MODE[0]!=(bool)Enabled.SWITCH_CAMERA_MODE[1]){
+if((bool)Enabled.SWITCH_CAMERA_MODE[0]){
 if(CamFollowables.Count>0){
 CamFollowing=CamFollowables.First.Value;CamFollowables.RemoveFirst();CamFollowables.AddLast(CamFollowing.CamFollowableNode);
 CamFollowing.BeingCamFollowed=true;
@@ -59,16 +60,16 @@ CamFollowing=null;
 }
 if(CamFollowing==null){
 #region FORWARD BACKWARD
-    if(Enabled.FORWARD [0]){inputMoveSpeed.z+=InputMoveAcceleration.z;} 
-    if(Enabled.BACKWARD[0]){inputMoveSpeed.z-=InputMoveAcceleration.z;}
-        if(!Enabled.FORWARD[0]&&!Enabled.BACKWARD[0]){inputMoveSpeed.z=0;}
+    if((bool)Enabled.FORWARD [0]){inputMoveSpeed.z+=InputMoveAcceleration.z;} 
+    if((bool)Enabled.BACKWARD[0]){inputMoveSpeed.z-=InputMoveAcceleration.z;}
+        if(!(bool)Enabled.FORWARD[0]&&!(bool)Enabled.BACKWARD[0]){inputMoveSpeed.z=0;}
             if( inputMoveSpeed.z>InputMaxMoveSpeed.z){inputMoveSpeed.z= InputMaxMoveSpeed.z;}
             if(-inputMoveSpeed.z>InputMaxMoveSpeed.z){inputMoveSpeed.z=-InputMaxMoveSpeed.z;}
 #endregion
 #region RIGHT LEFT
-    if(Enabled.RIGHT   [0]){inputMoveSpeed.x+=InputMoveAcceleration.x;} 
-    if(Enabled.LEFT    [0]){inputMoveSpeed.x-=InputMoveAcceleration.x;}
-        if(!Enabled.RIGHT[0]&&!Enabled.LEFT[0]){inputMoveSpeed.x=0;}
+    if((bool)Enabled.RIGHT   [0]){inputMoveSpeed.x+=InputMoveAcceleration.x;} 
+    if((bool)Enabled.LEFT    [0]){inputMoveSpeed.x-=InputMoveAcceleration.x;}
+        if(!(bool)Enabled.RIGHT[0]&&!(bool)Enabled.LEFT[0]){inputMoveSpeed.x=0;}
             if( inputMoveSpeed.x>InputMaxMoveSpeed.x){inputMoveSpeed.x= InputMaxMoveSpeed.x;}
             if(-inputMoveSpeed.x>InputMaxMoveSpeed.x){inputMoveSpeed.x=-InputMaxMoveSpeed.x;}
 #endregion
@@ -88,6 +89,7 @@ inputMoveSpeed=Vector3.zero;
                    base.Update();
 }
 #region UI-to-Command translating
+Vector3 _coordToSignedCoord{get;}=new Vector3(-Chunk.Width/2f,-Chunk.Height/2f,-Chunk.Depth/2f);Vector3 _adjToCenter{get;}=new Vector3(-.5f,-.5f,-.5f);
 public enum SelectedGameModeTool:int{
 CommandSim=0,
 TerrainCarveCube=1,
@@ -101,9 +103,34 @@ SwitchTool(LastTool,CurrentTool);
 LastTool=CurrentTool;
 }
 Ray ray=Camera.main.ScreenPointToRay(Input.mousePosition);
-Debug.DrawRay(ray.origin,ray.direction*1000f);
+
+
+Vector3 hitPoint;bool hitTerrain=false;
+if(Physics.Raycast(ray,out RaycastHit hit,1000f)){
+hitPoint=hit.point;hitTerrain=hit.collider.GetComponent<TerrainChunk>()!=null;
+}else{
+hitPoint=ray.origin+ray.direction*1000f;
+}
+
+
+Debug.DrawLine(ray.origin,hitPoint);
 switch(CurrentTool){
 case(SelectedGameModeTool.TerrainCarveCube):{
+
+
+Vector2Int cnkRgn=ChunkManager.PosToRgn(hitPoint);
+Vector3Int vCoord=Chunk.PosToCoord(hitPoint);
+Vector3 previewPos=vCoord+_coordToSignedCoord;
+        previewPos.x+=cnkRgn.x;
+        previewPos.z+=cnkRgn.y;
+    //Debug.LogWarning(previewPos);
+if(TerrainCarveCubeIndicators[0]!=null){TerrainCarveCubeIndicators[0].transform.position=previewPos+Vector3.Scale(_adjToCenter,TerrainCarveCubeIndicators[0].transform.lossyScale);TerrainCarveCubeIndicators[0].transform.rotation=Quaternion.identity;}
+if(hitTerrain){
+if((bool)Enabled.ACTION_1[0]&&(bool)Enabled.ACTION_1[0]!=(bool)Enabled.ACTION_1[1]){
+    Debug.LogWarning("Edit Action "+Enabled.ACTION_1[0]+" "+Enabled.ACTION_1[1]);
+//ChunkManager.main.Edit(previewPos,new Vector3Int(1,1,1),51,MaterialId.Dirt,ChunkManager.EditMode.Cube);
+}
+}
     break;
 }
 }
@@ -130,11 +157,11 @@ indicator.SetActive(true);
 
 
 #endregion
-[NonSerialized]string _name;[NonSerialized]object[]_command;[NonSerialized]bool[]_state;
+[NonSerialized]string _name;[NonSerialized]object[]_command;[NonSerialized]object[]_state;
 void UpdateCommandState(KeyValuePair<string,object[]>command){_name=command.Key;_command=command.Value;_state=AllStates[_name];
 _state[1]=_state[0];
 if(_command.Length==2&&_command[1]is string){
-_state[0]=Get(_command[0],mode:(string)_command[1],_state[0]);
+_state[0]=Get(_command[0],mode:(string)_command[1],(bool)_state[0]);
 }
 }
 bool Get(object command,string mode,bool currState){
@@ -151,15 +178,18 @@ return mode=="activeHeld"?(command is KeyCode?Input.GetKey    ((KeyCode)command)
 }
 }
 public static class Enabled{
-public static readonly bool[]PAUSE={true,true};
-public static readonly bool[]FORWARD ={false,false};
-public static readonly bool[]BACKWARD={false,false};
-public static readonly bool[]RIGHT   ={false,false};
-public static readonly bool[]LEFT    ={false,false};
-public static readonly bool[]JUMP    ={false,false};
+public static readonly object[]PAUSE={true,true};
+public static readonly object[]FORWARD ={false,false};
+public static readonly object[]BACKWARD={false,false};
+public static readonly object[]RIGHT   ={false,false};
+public static readonly object[]LEFT    ={false,false};
+public static readonly object[]JUMP    ={false,false};
 public static readonly float[]MOUSE_ROTATION_DELTA_X={0,0};
 public static readonly float[]MOUSE_ROTATION_DELTA_Y={0,0};
-public static readonly bool[]SWITCH_CAMERA_MODE={false,false};//  Free camera or follow and command a player
+public static readonly object[]SWITCH_CAMERA_MODE={false,false};//  Free camera or follow and command a player
+public static readonly object[]ACTION_1={false,false};
+public static readonly object[]ACTION_2={false,false};
+public static readonly object[]INTERACT={false,false,0f};
 }
 public static class Commands{
 public static object[]PAUSE={KeyCode.Tab,"alternateDown"};
@@ -171,4 +201,7 @@ public static object[]JUMP    ={KeyCode.E,"whenUp"};
 public static float ROTATION_SENSITIVITY_X=360.0f;
 public static float ROTATION_SENSITIVITY_Y=360.0f;
 public static object[]SWITCH_CAMERA_MODE={KeyCode.RightAlt,"alternateDown"};  //  Free or following the player
+public static object[]ACTION_1={(int)0,"activeHeld"};
+public static object[]ACTION_2={(int)1,"activeHeld"};
+public static object[]INTERACT={KeyCode.G,"holdDelay",1f};//  So you can't, for example, "steal an item" instantly
 }
