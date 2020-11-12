0e51cf0e0e50f49471ed8e9a7dc220594f48a698
Arthur Kenichi Condino Odaguiri <46831317+Arthur-Kenichi-Condino@users.noreply.github.com>
2020-11-12 03:38:55 -0300

    run animation

--- Assets/AKCondinoO/Chares/Animators/AnimatorParamChanger.cs
+++ Assets/AKCondinoO/Chares/Animators/AnimatorParamChanger.cs
@@ -7,12 +7,18 @@ public class AnimatorParamChanger:MonoBehaviour{
 void OnEnable(){
 actor=GetComponent<AI>();
 }
+[NonSerialized]Vector3 _horizontalMoveSpeed;[NonSerialized]public float horizontalMoveSensibility=1f/3f;[NonSerialized]public bool backwardAvailable=false;
 void Update(){
 if(actor.IsUMA&&animator==null){
 animator=GetComponentInChildren<Animator>();
 
     Debug.LogWarning(actor+";animator:"+animator);
 
+}
+if(animator!=null){
+_horizontalMoveSpeed=actor.rigidbody.velocity;
+_horizontalMoveSpeed.y=0;
+animator.SetFloat("Forward",_horizontalMoveSpeed.magnitude*(backwardAvailable&&Vector3.Angle(actor.rigidbody.transform.forward,actor.rigidbody.velocity.normalized)>90?-1:1)*horizontalMoveSensibility,0.1f,Time.deltaTime);
 }
 }
 }
